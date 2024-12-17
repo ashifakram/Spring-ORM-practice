c565778a3f67f0e671d8c6912eaf223f963c6f2c
ashifakram <ashifakram1999@gmail.com>
2024-12-17 19:33:51 +0530

    Added spring-orm-practice

---
+++ src/main/java/org/jsp/hibernatetemplatepractice/TestCfg.java
@@ -0,0 +1,19 @@
+package org.jsp.hibernatetemplatepractice;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+public class TestCfg {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-orm.xml");
+		HibernateTemplate template = context.getBean("hibernateTemplate", HibernateTemplate.class);
+		System.out.println(template);
+		
+		((ClassPathXmlApplicationContext) context).close();
+	}
+
+}
